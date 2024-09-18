8d187f566b769f9466931219559d60273cfadc93
jeff821-vs <jmhill821@frontier.com>
2024-09-18 08:11:25 -0400

    added a player object to the center of the screen

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
+from circleshape import *
 
 def main():
     pygame.init()
+    fps = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+ 
+    
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+        fps.tick(60)
+        dt += (fps.get_time() / 1000)
+        
 
 
 if __name__ == "__main__":
